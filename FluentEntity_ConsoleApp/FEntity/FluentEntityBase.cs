6bdc85f6ba90a070b31987d3ce3f3f3595fc3736
emir57 <emir.gurbuz06@hotmail.com>
2022-06-16 15:22:50 +0300

    throw FluentException

--- FluentEntity_ConsoleApp/FEntity/FluentEntityBase.cs
+++ FluentEntity_ConsoleApp/FEntity/FluentEntityBase.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FluentEntity_ConsoleApp.Exceptions;
+using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Reflection;
@@ -18,12 +19,18 @@ namespace FluentEntity_ConsoleApp.FEntity
         {
             string propertyName = (exp.Body as MemberExpression).Member.Name;
             PropertyInfo propertyInfo = entity.GetType().GetProperty(propertyName);
+
+            if (propertyInfo == null) throw new FluentException("Property Not Found");
+
             propertyInfo.SetValue(entity, value);
             return this;
         }
         public virtual FluentEntityBase<T> AddParameter(string propertyName, object value)
         {
             PropertyInfo propertyInfo = entity.GetType().GetProperty(propertyName);
+
+            if (propertyInfo == null) throw new FluentException("Property Not Found");
+
             propertyInfo.SetValue(entity, value);
             return this;
         }
