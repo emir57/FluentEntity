a282c1650cb3f3f6e77bf806f4d8deb60672803b
emir57 <emir.gurbuz06@hotmail.com>
2022-06-16 15:06:23 +0300

    Create FluentEntityBase

---
+++ FluentEntity_ConsoleApp/FEntity2/FluentEntityBase.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+
+namespace FluentEntity_ConsoleApp.FEntity2
+{
+    public abstract class FluentEntityBase<T>
+        where T : class, new()
+    {
+        protected T entity;
+        public FluentEntityBase()
+        {
+            entity = new T();
+        }
+        public FluentEntityBase<T> AddParameter<P>(Expression<Func<T, P>> exp, object value)
+        {
+            string propertyName = (exp.Body as MemberExpression).Member.Name;
+            PropertyInfo propertyInfo = entity.GetType().GetProperty(propertyName);
+            propertyInfo.SetValue(entity, value);
+            return this;
+        }
+
+        public T GetEntity()
+        {
+            return entity;
+        }
+    }
+}
