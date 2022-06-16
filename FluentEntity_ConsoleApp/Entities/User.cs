71f294d03c9e9b7fb8e222bcc60bf4d14bf68b14
emir57 <emir.gurbuz06@hotmail.com>
2022-06-16 14:57:58 +0300

    Create User

---
+++ FluentEntity_ConsoleApp/Entities/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FluentEntity_ConsoleApp.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public bool EmailConfirm { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+    }
+}
