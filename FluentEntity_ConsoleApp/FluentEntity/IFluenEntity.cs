e1923c085a5a5b88880c17e1ff92c80bc903d50f
emir57 <emir.gurbuz06@hotmail.com>
2022-06-16 14:51:42 +0300

    IFluenEntity -> AddParameter

---
+++ FluentEntity_ConsoleApp/FluentEntity/IFluenEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace FluentEntity_ConsoleApp.FluentEntity
+{
+    public interface IFluenEntity<T>
+    {
+        IFluenEntity<T> AddParameter<P>(Expression<Func<T, P>> exp, object value);
+    }
+}
