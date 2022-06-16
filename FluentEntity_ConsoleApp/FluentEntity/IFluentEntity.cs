283e77cb8ed6e730355dd72b810bce6f8517b5fc
emir57 <emir.gurbuz06@hotmail.com>
2022-06-16 14:55:41 +0300

    IFluentEntity where T

--- FluentEntity_ConsoleApp/FluentEntity/IFluentEntity.cs
+++ FluentEntity_ConsoleApp/FluentEntity/IFluentEntity.cs
@@ -6,6 +6,7 @@ using System.Text;
 namespace FluentEntity_ConsoleApp.FluentEntity
 {
     public interface IFluentEntity<T>
+        where T : class, new()
     {
         IFluentEntity<T> AddParameter<P>(Expression<Func<T, P>> exp, object value);
         T GetEntity();
