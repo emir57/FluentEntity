803d7a19d5104e0b1c1672b7505bb182847c0775
emir57 <emir.gurbuz06@hotmail.com>
2022-06-16 15:20:06 +0300

    Create FluentException

---
+++ FluentEntity_ConsoleApp/Exceptions/FluentException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace FluentEntity_ConsoleApp.Exceptions
+{
+    public class FluentException : Exception
+    {
+        public FluentException() : base("FluentEntity Exception")
+        {
+        }
+
+        public FluentException(string message) : base(message)
+        {
+        }
+
+        public FluentException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
