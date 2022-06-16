88809ada5affbe3b6723fcc830ed523330f5c00e
emir57 <emir.gurbuz06@hotmail.com>
2022-06-16 14:54:16 +0300

    AddParameter

---
+++ FluentEntity_ConsoleApp/FluentEntity/FluentEntity.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+
+namespace FluentEntity_ConsoleApp.FluentEntity
+{
+    public class FluentEntity<T> : IFluentEntity<T>
+    {
+        private T entity;
+        public FluentEntity()
+        {
+            entity = new T();
+        }
+        public IFluentEntity<T> AddParameter<P>(Expression<Func<T, P>> exp, object value)
+        {
+            var propertyName = (exp.Body as MemberExpression).Member.Name;
+            PropertyInfo propertyInfo = entity.GetType().GetProperty(propertyName);
+            propertyInfo.SetValue(entity, value);
+            return this;
+        }
+
+        public T GetEntity()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
