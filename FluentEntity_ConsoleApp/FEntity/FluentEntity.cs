abf6de7d46f3d0ad96dc8e9ae4f1e81aaff48edb
emir57 <emir.gurbuz06@hotmail.com>
2022-06-16 15:09:40 +0300

    delete override function in FluentEntity

--- FluentEntity_ConsoleApp/FEntity/FluentEntity.cs
+++ FluentEntity_ConsoleApp/FEntity/FluentEntity.cs
@@ -7,9 +7,5 @@ namespace FluentEntity_ConsoleApp.FEntity
     public class FluentEntity<T> : FluentEntityBase<T>
         where T : class, new()
     {
-        public override FluentEntityBase<T> AddParameter<P>(System.Linq.Expressions.Expression<Func<T, P>> exp, object value)
-        {
-            return base.AddParameter(exp, value);
-        }
     }
 }
